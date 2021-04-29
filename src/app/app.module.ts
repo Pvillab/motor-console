8e9fe5b04b5c3c2670244a3d602d6b9c0cedf66d
Paul Villafuerte Beita <paulvillabeita@gmail.com>
2021-04-28 21:48:46 -0600

    Routing implemented

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { AppRoutingModule } from './app-routig.module';
 
 import { AppComponent } from './app.component';
+import { ConsoleComponent } from './console/console.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ConsoleComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
